15d2bf625537dec30212692285e6a9ce12e445ff
Maciej Wasiak <mwasiak154@gmail.com>
2020-09-25 14:25:35 +0200

    projects section js

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,46 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+
+function addProject(title, description, demoUrl, githubUrl) {
+    const projects = document.querySelector('.projects');
+    let content = `
+    <article class="project">
+        <div class="project__titlebar">• • •</div>
+        <img class="project__github-logo" src="../../public/github.svg" alt="github-logo"/>
+        <div class="project__content">
+            <span>project:</span>
+            <span class="project__content--title">${title}</span>
+            <span>description:</span>
+            <span class="project__content--description">${description}</span>
+            <span>demo:</span>
+            <span class="project__content--demo"><<a href="${demoUrl}">link</a>></span>
+            <span>github:</span>
+            <span class="project__content--github"><<a href="${githubUrl}">link</a>></span>
+        </div>
+    </article>
+    `;
+    projects.innerHTML += content;
+}
+
+// addProject('bla', 'ble', 'bli', 'blo');
+
+fetch('https://api.github.com/users/maciej-wasiak/repos?sort=created')
+    .then(resp => resp.json())
+    .then(resp => {
+        resp = resp.filter(e => e['name'] != 'maciej-wasiak.github.io');
+        if(resp.length > 4){
+            for (let i = 0; i < 4; i++) {
+                const {name, html_url, description} = repo;
+                addProject(name, description, `https://maciej-wasiak.github.io/${name}`, html_url);
+            }
+        } else {
+            for (let repo of resp) {
+                const {name, html_url, description} = repo;
+                addProject(name, description, `https://maciej-wasiak.github.io/${name}`, html_url);
+            }
+        }
+    })
+    .catch(error => {
+        console.log(`Error - API: ${error}`);
+    })
